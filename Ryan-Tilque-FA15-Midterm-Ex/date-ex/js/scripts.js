cb6a820ed5d0f659d5cd63b989121d066891afd6
Ryan Tilque <rtilque@engr-134-11.eastern.ewu.edu>
2015-11-05 12:58:10 -0800

    1.0

---
+++ Ryan-Tilque-FA15-Midterm-Ex/date-ex/js/scripts.js
@@ -0,0 +1,20 @@
+// REFER TO: 
+// text() : http://api.jquery.com/text/
+// toDateString() : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toDateString
+// getFullYear()  : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/getFullYear
+var date = new Date();
+
+$('.todays-date').text(date.toDateString());
+
+// Construct Date() object for today date & this year
+
+
+
+// Use Date() methods. Reasign/store new values.
+
+
+
+// Dynamically insert todays date
+
+
+// Dynamically insert this year
