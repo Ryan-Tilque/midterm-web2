371d5c042c0e97db8720f295da3ff9dfad93db0d
Ryan Tilque <21rryan@gmail.com>
2015-11-10 00:03:32 -0800

    3.0
    Finished day night mode

--- Ryan-Tilque-FA15-Midterm-Ex/day-night-mode-ex/js/scripts.js
+++ Ryan-Tilque-FA15-Midterm-Ex/day-night-mode-ex/js/scripts.js
@@ -4,18 +4,25 @@
 // removeClass() : http://api.jquery.com/removeClass/
 
 // On click Night Button....
+	$('.night-btn').click(function() {
 
-  
   // Remove Day Class
-  
+  $('body').removeClass('day');
+
   // Add Night Class
+  $('body').addClass('night');
 
 
+});
 
 // On click Day Button....
+	$('.day-btn').click(function() {
 
-  
   // Remove Day Class
-  
+  $('body').removeClass('night');
+
   // Add Night Class
-  
+  $('body').addClass('day');
+
+
+});
